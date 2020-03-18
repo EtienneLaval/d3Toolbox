1c16dc1ff8f8e85436719d4d95a4089ce308db7e
EtienneLaval <etienne.m.laval@gmail.com>
2020-03-18 22:15:56 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <color-splitter msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import ColorSplitter from './components/ColorSplitter.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    ColorSplitter,
   },
-};
+}
 </script>
 
 <style lang="scss">
