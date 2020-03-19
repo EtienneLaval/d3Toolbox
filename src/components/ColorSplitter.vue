c529e616b14fa0472c15b87c8f2a8c44761e58b0
EtienneLaval <etienne.m.laval@gmail.com>
2020-03-19 22:33:01 +0100

    feat(colorSplitter) add trivial element gathering and color splitting tools

--- src/components/ColorSplitter.vue
+++ src/components/ColorSplitter.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="colorSplitter">
-    <div class="colorSplitter__set" v-for="set in sets" :key="set.toString()">
+    <div class="colorSplitter__set" v-for="(set,setIndex) in sets" :key="setIndex">
       <div
       class="colorSplitter__element"
-      v-for="element in set"
-      :key="set.toString()+element">
+      v-for="(element,elementIndex) in set"
+      :key="setIndex+'.'+elementIndex">
         {{element}}
       </div>
     </div>
@@ -23,12 +23,42 @@ export default {
         ['a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
         ['r', 't', 'y', 'u', 'i', 'o', 'p', 'q'],
       ],
+      keyColors: [
+        [127, 255, 0],
+        [0, 250, 154],
+        [199, 21, 133],
+      ],
+      minColorSpace: 50,
     }
   },
   computed: {
-    uniqueElements() {
-      return this.data
+    allElements() {
+      return this.sets.reduce((acc, set) => [...acc, ...set], [])
     },
+    uniqueElements() {
+      return this.allElements.filter(this.filterUnique)
+    },
+
+  },
+  methods: {
+    filterUnique(value, index, self) {
+      return self.indexOf(value) === index
+    },
+    getColorVector(KeyColor0, KeyColor1) {
+      return [
+        KeyColor1[0] - KeyColor0[0],
+        KeyColor1[1] - KeyColor0[1],
+        KeyColor1[2] - KeyColor0[2],
+      ]
+    },
+    getColorVectorNorm(colorVector) {
+      return Math.sqrt(
+        colorVector[0] * colorVector[0]
+        + colorVector[1] * colorVector[1]
+        + colorVector[2] * colorVector[2],
+      )
+    },
+
   },
 }
 </script>
